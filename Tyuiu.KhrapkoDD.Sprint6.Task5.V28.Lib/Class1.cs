da6c6d65d290d5ee6dd43f8075cd776cefd87af7
ChertVasiliy <kolyaminigan21@gmail.com>
2025-12-26 17:25:03 +0500

    вп

--- Tyuiu.KhrapkoDD.Sprint6.Task5.V28.Lib/Class1.cs
+++ Tyuiu.KhrapkoDD.Sprint6.Task5.V28.Lib/Class1.cs
@@ -1,7 +1,28 @@
-﻿namespace Tyuiu.KhrapkoDD.Sprint6.Task5.V28.Lib
-{
-    public class Class1
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
+namespace Tyuiu.KhrapkoDD.Sprint6.Task5.V28.Lib
+{
+    public class DataService : ISprint6Task5V28
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            // Читаем все строки из файла
+            string[] lines = File.ReadAllLines(path);
+
+            // Преобразуем строки в числа, фильтруем их и округляем
+            double[] numsArray = lines
+                .Select(line => Convert.ToDouble(line))
+                .Select(val => Math.Round(val, 3)) // Округляем до 3 знаков после запятой
+                .Where(val => val < 10) // Оставляем только числа меньше 10
+                .ToArray();
+
+            // Выводим результат
+            foreach (var num in numsArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            return numsArray;
+        }
     }
 }
