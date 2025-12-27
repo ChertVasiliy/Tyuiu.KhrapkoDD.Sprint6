f0fa9c61df8725d99a68d8a047ed774588e2b712
ChertVasiliy <kolyaminigan21@gmail.com>
2025-12-27 06:22:18 +0500

    u

--- Tyuiu.KhrapkoDD.Sprint6.Task6.V28.Lib/Class1.cs
+++ Tyuiu.KhrapkoDD.Sprint6.Task6.V28.Lib/Class1.cs
@@ -1,7 +1,24 @@
-﻿namespace Tyuiu.KhrapkoDD.Sprint6.Task6.V28.Lib
-{
-    public class Class1
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
+namespace Tyuiu.KhrapkoDD.Sprint6.Task6.V28.Lib
+{
+
+    public class DataService : ISprint6Task6V28
+    {
+        public string CollectTextFromFile(string path)
+        {
+            var ret = String.Empty;
+
+            using (StreamReader sr = new(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line != String.Empty)
+                        ret += $" {line.Split(' ')[^2]}";
+                }
+            }
+            return ret[1..];
+        }
     }
 }
